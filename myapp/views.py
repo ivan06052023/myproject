4da9015e63899ff96bd44518e2de2a08891f076e
ivan06052023 <132770575+ivan06052023@users.noreply.github.com>
2023-07-06 15:39:53 +0300

    .\myproject\

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def page1(request):
+    return render(request, 'page1.html')
+
+def page2(request):
+    return render(request, 'page2.html')
+
+def page3(request):
+    return render(request, 'page3.html')
+
+# Create your views here.
